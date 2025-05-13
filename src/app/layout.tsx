8bc701102a5b476dfbad705afd9866e952fa8352
Kirill Scherba <kiriushka@gmail.com>
2025-05-13 23:01:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:. Error source: - A server/client branch `if (typeof window !== 'undefined')`.
    > - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    > - Date formatting in a user's locale which doesn't match the server.
    > - External changing data without sending a snapshot of it along with the HTML.
    > - Invalid HTML tag nesting.
    >
    > It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    >
    > See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >   ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    >                       <html
    >                         lang="en"
    >                         className="geistsans_81192321-module__keQz_a__variable geistmono_8e2790ea-module__fuNg9a__vari..."
    > -                       data-bybit-channel-name="yKbo_Fiq5Ory5JpW19TPy"
    > -                       data-bybit-is-default-wallet="true"
    >                       >
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (18:5) @ RootLayout
    >
    >   16 | }>) {
    >   17 |   return (
    > > 18 |     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
    >      |     ^
    >   19 |       <body className="font-sans antialiased">
    >   20 |         {children}
    >   21 |         <Toaster />
    >
    > Call Stack
    > 17
    >
    > Show 15 ignore-listed frame(s)
    > html
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (18:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning={true}>
       <body className="font-sans antialiased">
         {children}
         <Toaster />
