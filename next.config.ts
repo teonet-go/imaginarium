fe7609ca6e1321fa6808aefddf9a99f31e380e8a
Kirill Scherba <kirill@scherba.ru>
2025-05-15 13:48:20 +0300

    Allow placehold.co images

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
